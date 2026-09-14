a267e11a66c0e3ca4b8c671892f95b3d3fa184f5
aadhyabhadana-prog <aadhyabhadana@gmail.com>
2026-09-14 20:34:29 +0530

    Add 2d matrix addition program

---
+++ 2d_matrix_addition.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    int i, j;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter the number of columns: ");
+    scanf("%d", &cols);
+    int matrix1[rows][cols];
+    int matrix2[rows][cols];
+    int sum[rows][cols];
+    printf("\nEnter elements for the 1st matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("Element [%d][%d]: ", i, j);
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+    printf("\nEnter elements for the 2nd matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("Element [%d][%d]: ", i, j);
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            sum[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+    printf("\nResultant Sum Matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%d\t", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
